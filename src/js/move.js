32f3eeda3c1cb22ef871d021433b32830fd02c2f
Dmitry Esin <dmitry.esin@gmail.com>
2016-01-08 00:58:33 +0300

    js and jade templates modified

---
+++ src/js/move.js
@@ -0,0 +1,47 @@
+;var moveModule = (function () {
+
+	var init = function () {
+		_setUpListners();
+		// do! after initialization
+	};
+
+	var _setUpListners = function () {
+		// listening 
+	}
+
+	return { // public methods
+		init: init
+	};
+
+})();
+
+moveModule.init();
+
+
+
+
+// $(functio() {
+
+//   $(".settings-box__img-position-coordinates_x").append('<div class="inc button">+</div><div class="dec button">-</div>');
+
+//   $(".button").on("click", function() {
+
+//     var $button = $(this);
+//     var oldValue = $button.parent().find("input").val();
+
+//     if ($button.text() == "+") {
+//   	  var newVal = parseFloat(oldValue) + 1;
+//   	} else {
+// 	   // Don't allow decrementing below zero
+//       if (oldValue > 0) {
+//         var newVal = parseFloat(oldValue) - 1;
+// 	    } else {
+//         newVal = 0;
+//       }
+// 	  }
+
+//     $button.parent().find("input").val(newVal);
+
+//   });
+
+// });
