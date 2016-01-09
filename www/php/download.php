e53c28a454469e1d229fa73ea0fc8577f5f18369
Slava Zuryanov <slava.zuryanov@gmail.com>
2016-01-09 15:41:43 +0200

    Добавлены php в www/php

---
+++ www/php/download.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../../composer.local/vendor/autoload.php";
+require_once "validation.php";
+use \WideImage\WideImage as WideImage;
+
+$image = WideImage::loadFromFile('img_upload');
