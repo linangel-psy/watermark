8473927ba2246c5fd891d53be532711ea89550fd
linangel.psy <linangel.psy@gmail.com>
2016-01-09 22:27:37 +0000

    размещение изображения, перемещение "сеткой"

--- src/js/image.js
+++ src/js/image.js
@@ -8,11 +8,11 @@ var imgSizeCalculation = function(box, boxH, boxW, imgH, imgW) {
 		if (proportionsW > proportionsH) {
 			var height = (boxW / imgW) * imgH;
 			setBoxSize(box, height, boxW);
-			setPosition(box, boxH, boxW, height, boxW);
+			setPosition(box, boxH, boxW, height, boxW, 'middle-middle');
 		} else {
 			var width = (boxH / imgH) * imgW;
 			setBoxSize(box, boxH, width);
-			setPosition(box, boxH, boxW, boxH, width);
+			setPosition(box, boxH, boxW, boxH, width, 'middle-middle');
 		}
 	};
 };
@@ -24,11 +24,76 @@ var setBoxSize = function(box, insideH, insideW) {
 	});
 };
 
-var setPosition = function(box, outH, outW, insideH, insideW) {
-	var top = (outH - insideH) / 2,
-	left = (outW - insideW) / 2;
-	$(box).find('img').css({
-		'top': top,
-		'left': left
-	});
+var setPosition = function(box, outH, outW, insideH, insideW, id) {
+	outW = parseInt(outW);
+	outH = parseInt(outH);
+	insideW = parseInt(insideW);
+	insideH = parseInt(insideH);
+	switch(id) {
+		case 'top-left':
+			$(box).css({
+				'top': 0,
+				'left': 0
+			});
+			break;
+		case 'top-middle':
+			$(box).css({
+				'top': 0,
+				'left': (outW - insideW) / 2
+			});
+			break;
+		case 'top-right':
+			$(box).css({
+				'top': 0,
+				'left': (outW - insideW)
+			});
+			break;
+		case 'middle-left':
+			$(box).css({
+				'top': (outH - insideH) / 2,
+				'left': 0
+			});
+			break;
+		case 'middle-middle':
+			$(box).css({
+				'top': (outH - insideH) / 2,
+				'left': (outW - insideW) / 2
+			});
+			break;
+		case 'middle-right':
+			$(box).css({
+				'top': (outH - insideH) / 2,
+				'left': (outW - insideW)
+			});
+			break;
+		case 'bottom-left':
+			$(box).css({
+				'top': (outH - insideH),
+				'left': 0
+			});
+			break;
+		case 'bottom-middle':
+			$(box).css({
+				'top': (outH - insideH),
+				'left': (outW - insideW) / 2
+			});
+			break;
+		case 'bottom-right':
+			$(box).css({
+				'top': (outH - insideH),
+				'left': (outW - insideW)
+			});
+			break;
+	}
 };
+
+$('.settings-box__link').click(function(){
+	$('.settings-box__link').removeClass('active');
+	$(this).addClass('active');
+	var id = $(this).attr('id'),
+		outH = $('.img-box').css('height'),
+		outW = $('.img-box').css('width'),
+		insideH = $('.watermark-box').css('height'),
+		insideW = $('.watermark-box').css('width');
+	setPosition('#watermarkBox', outH, outW, insideH, insideW, id)
+})
