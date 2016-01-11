c026de8bdcab9ba24330ee0507b859ba409f585e
linangel.psy <linangel.psy@gmail.com>
2016-01-11 20:22:01 +0000

    исправлена прозрачность

--- src/js/move.js
+++ src/js/move.js
@@ -11,12 +11,12 @@ var spinnerX = $( "#coordinatesX" ).spinner({
 	spin: function( event, ui ) {
 		maxX = max[0];
 		$( "#coordinatesX" ).spinner({min: 0, max: maxX});
-		if ( ui.value <= maxX && ui.value >= 0 ) {
+		if ( ui.value < maxX && ui.value > 0 ) {
 			ui.value += 1;
 		}
-		else if ( ui.value > maxX ) {
+		else if ( ui.value >= maxX ) {
 			ui.value = maxX;
-		} else if ( ui.value < 0 ) {
+		} else if ( ui.value <= 0 ) {
 			ui.value = 0;
 		}
 		$("#watermarkBox").css("left", ui.value);
@@ -29,12 +29,12 @@ var spinnerY = $( "#coordinatesY" ).spinner({
 	spin: function( event, ui ) {
 		maxY = max[1];
 		$( "#coordinatesY" ).spinner({min: 0, max: maxY});
-		if ( ui.value <= maxY && ui.value >= 0 ) {
+		if ( ui.value < maxY && ui.value > 0 ) {
 			ui.value -= 1;
 		}
-		else if ( ui.value > maxY ) {
+		else if ( ui.value >= maxY ) {
 			ui.value = maxY;
-		} else if ( ui.value < 0 ) {
+		} else if ( ui.value <= 0 ) {
 			ui.value = 0;
 		}
 		$("#watermarkBox").css("top", ui.value);
