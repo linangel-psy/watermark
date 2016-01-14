18532d057f8350c18d5e40946d899f08cc2f98e3
Slava Zuryanov <slava.zuryanov@gmail.com>
2016-01-14 22:11:01 +0200

    Готовый легкий уровень сложности

--- www/php/image.php
+++ www/php/image.php
@@ -17,33 +17,14 @@ $main_image->addLayer(1, $watermark, $x_coordinates, $y_coordinates, "LT");
 
 $random = rand(0, getrandmax());
 
-$dirPath = $_SERVER['DOCUMENT_ROOT'].'/www/img/uploads/';
+$dirPath = '../img/uploads/';
 $filename = 'team6-'.$random.'.png';
 $createFolders = false;
 $imageQuality = 50;
 
 $main_image->save($dirPath, $filename, $createFolders, $imageQuality);
 
-/*
-list($main_image_width, $main_image_height) = getimagesize($main_image_path);
-list($watermark_width, $watermark_height) = getimagesize($watermark_path);
-
-if($main_image_width>650){
-    $uploaded_main_image_width = $x_coordinates + $watermark_width;
-    $coefficient = $main_image_width/$uploaded_main_image_width;
-}
-
-else if($main_image_height>530 && !$coefficient){
-    $uploaded_main_image_height = $y_coordinates + $watermark_height;
-    $coefficient = $main_image_height/$uploaded_main_image_height;
-}
-
-else{
-    $coefficient = 1;
-}
-*/
-
-$main_image = $_SERVER['DOCUMENT_ROOT'].'/www/img/uploads/'.$filename;
+$main_image = '../img/uploads/'.$filename;
 
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
@@ -51,8 +32,6 @@ header('Content-Disposition: attachment; filename='.basename($main_image));
 header('Content-Length: '.filesize($main_image));
 
 readfile($main_image);
-
-array_map( "unlink", glob('../img/uploads/*') );
 
 header('Location: '.$_SERVER['DOCUMENT_ROOT'].'/www/');
 
